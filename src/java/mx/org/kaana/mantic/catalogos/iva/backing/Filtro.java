375a6bc65ac4a83e02754894c78d16ed940c0b51
team.developer <team.developer@gmail.com>
2018-05-01 00:17:33 -0500

    se agrego el filtro para el catalogo del iva ademas se dejo como mensaje por defecto el inline

---
+++ src/java/mx/org/kaana/mantic/catalogos/iva/backing/Filtro.java
@@ -0,0 +1,73 @@
+package mx.org.kaana.mantic.catalogos.iva.backing;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import mx.org.kaana.kajool.db.comun.sql.Entity;
+import mx.org.kaana.libs.formato.Error;
+import mx.org.kaana.kajool.enums.EAccion;
+import mx.org.kaana.kajool.enums.EFormatoDinamicos;
+import mx.org.kaana.kajool.reglas.comun.Columna;
+import mx.org.kaana.kajool.reglas.comun.FormatCustomLazy;
+import mx.org.kaana.libs.Constantes;
+import mx.org.kaana.libs.pagina.IBaseFilter;
+import mx.org.kaana.libs.pagina.JsfBase;
+import mx.org.kaana.libs.pagina.UIBackingUtilities;
+import mx.org.kaana.libs.reflection.Methods;
+
+@Named(value = "manticCatalogosIvaFiltro")
+@ViewScoped
+public class Filtro extends IBaseFilter implements Serializable {
+
+  private static final long serialVersionUID = 8793667741599428369L;
+
+  @PostConstruct
+  @Override
+  protected void init() {
+    try {
+      this.attrs.put("sortOrder", "order by tc_mantic_historial_iva.registro");
+      this.attrs.put("importe", "");
+    } // try
+    catch (Exception e) {
+      Error.mensaje(e);
+      JsfBase.addMessageError(e);
+    } // catch		
+  } // init
+ 
+  @Override
+  public void doLoad() {
+    List<Columna> campos = null;
+    try {
+      campos = new ArrayList<>();
+      campos.add(new Columna("importe", EFormatoDinamicos.NUMERO_CON_DECIMALES));
+      campos.add(new Columna("registro", EFormatoDinamicos.DIA_FECHA_HORA));      
+      this.lazyModel = new FormatCustomLazy("TcHistorialIvaDto", "importe", this.attrs, campos);
+      UIBackingUtilities.resetDataTable();
+    } // try
+    catch (Exception e) {
+      Error.mensaje(e);
+      JsfBase.addMessageError(e);
+    } // catch
+    finally {
+      Methods.clean(campos);
+    } // finally		
+  } // doLoad
+
+  public String doAccion(String accion) {
+    EAccion eaccion= null;
+		try {
+			eaccion= EAccion.valueOf(accion.toUpperCase());
+			JsfBase.setFlashAttribute("accion", eaccion);		
+			JsfBase.setFlashAttribute("retorno", "/Paginas/Mantic/Catalogos/Iva/filtro");		
+			JsfBase.setFlashAttribute("idHistorialIva", eaccion.equals(EAccion.MODIFICAR) ? ((Entity)this.attrs.get("seleccionado")).getKey() : -1L);
+		} // try
+		catch (Exception e) {
+			Error.mensaje(e);
+			JsfBase.addMessageError(e);			
+		} // catch
+		return "/Paginas/Mantic/Catalogos/Iva/accion".concat(Constantes.REDIRECIONAR);
+  } // doAccion  
+}
