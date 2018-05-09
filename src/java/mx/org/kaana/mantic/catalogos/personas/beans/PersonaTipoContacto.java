7e8c4b611747a7179e1082e7929539a6d6c3a445
OneDeveloper2016 <>
2018-05-09 00:26:14 -0500

    Se agrego el aributo nombre al bean

--- src/java/mx/org/kaana/mantic/catalogos/personas/beans/PersonaTipoContacto.java
+++ src/java/mx/org/kaana/mantic/catalogos/personas/beans/PersonaTipoContacto.java
@@ -8,6 +8,7 @@ public class PersonaTipoContacto extends TrManticPersonaTipoContactoDto{
 	private static final long serialVersionUID = 4804251646663919847L;
 	private ESql sqlAccion;
 	private Boolean nuevo;
+	private String nombreCompleto;
 
 	public PersonaTipoContacto() {
 		this(-1L);
@@ -18,13 +19,14 @@ public class PersonaTipoContacto extends TrManticPersonaTipoContactoDto{
 	}
 	
 	public PersonaTipoContacto(Long key, ESql sqlAccion) {
-		this(key, sqlAccion, false);
+		this(key, sqlAccion, false, null);
 	}
 	
-	public PersonaTipoContacto(Long key, ESql sqlAccion, Boolean nuevo) {
+	public PersonaTipoContacto(Long key, ESql sqlAccion, Boolean nuevo, String nombreCompleto) {
 		super(key);
-		this.sqlAccion= sqlAccion;
-		this.nuevo    = nuevo;
+		this.sqlAccion     = sqlAccion;
+		this.nuevo         = nuevo;
+		this.nombreCompleto= nombreCompleto;
 	}
 
 	public ESql getSqlAccion() {
@@ -41,5 +43,13 @@ public class PersonaTipoContacto extends TrManticPersonaTipoContactoDto{
 
 	public void setNuevo(Boolean nuevo) {
 		this.nuevo = nuevo;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
 	}
 }
